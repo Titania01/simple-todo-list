bee1f6115904a8a5e5d9c43e267838b37e8c0fb4
Titania01 <adegbitebidmus@gmail.com>
2020-12-26 12:58:11 +0100

    update code base

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 let addTodoButton = document.getElementsByClassName('addToDo');
+let toDoContainer = document.getElementsById('addToDoContainer');
+let inputField = document.getElementsByClassName('addToDo');
+
+addTodoButton.addEventListener('click', function(){
+  let paragraph = document.createElement('p')
+  paragraph.innerText = inputField.value;
+  toDoContainer.appendChild(paragraph);
+})
